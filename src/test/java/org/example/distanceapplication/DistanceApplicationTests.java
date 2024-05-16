23d1c85b2901fc2368336cb45c0f24dd1e288a79
maxiparshu <132914202+maxiparshu@users.noreply.github.com>
2024-05-16 13:45:37 +0300

    test rebuilding

--- src/test/java/org/example/distanceapplication/DistanceApplicationTests.java
+++ src/test/java/org/example/distanceapplication/DistanceApplicationTests.java
@@ -2,12 +2,14 @@ package org.example.distanceapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DistanceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+    String asd = "sonarqube";
+    assertNotNull(asd);
+  }
 
 }
